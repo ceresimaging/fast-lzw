87e3e7d318dd0a00c5f522ac096bf069300e6a56
Seth Nickell <snickell@gmail.com>
2019-07-29 11:57:37 -1000

    Be explicit about .js on import in case WASM is output

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import lzwWASM from './lzw-wasm'
+import lzwWASM from './lzw-wasm.js'
 import { expose, Transfer } from "threads/worker"
 
 const Module = lzwWASM()
@@ -9,17 +9,17 @@ async function decompress(typedArray) {
   const src = Module._malloc(typedArray.byteLength)
   const heapBytes = new Uint8Array(Module.HEAPU8.buffer, src, typedArray.byteLength)
   heapBytes.set(new Uint8Array(typedArray.buffer, typedArray.byteOffset, typedArray.byteLength))
-
+  
   const outSizePtr = Module._malloc(4)
   const outPtr = Module.ccall('lzwDecompress', 'number', ['number', 'number', 'number'], [heapBytes.byteOffset, heapBytes.byteLength, outSizePtr])
-
+  
   const outSize = new Uint32Array(Module.HEAPU8.buffer, outSizePtr, 4)[0]
-
+  
   const out = new Uint8Array(Module.HEAPU8.buffer, outPtr, outSize)
   
   const response = new Uint8Array(outSize)
   response.set(out)
-
+  
   Module._free(outPtr)
   Module._free(outSizePtr)
   
